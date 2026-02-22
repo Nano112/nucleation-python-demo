08e7053f2aa02ab0a748febc452a03836ab81a0d
Harrison <35530362+Nano112@users.noreply.github.com>
2026-02-23 03:49:57 +1100

    update schematic saving logic; add new output files and update dependencies

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 import time
 import os
 from nucleation import Schematic, ResourcePack, SchematicBuilder
+import nucleation
 resource_pack = ResourcePack.from_file("pack.zip")
 schem = Schematic("stone")
 
 XOR = ""
 with open("xor.txt", "r") as f:
     XOR = f.read()
-    
-schematic = (SchematicBuilder()
+
+schematic = (
+    SchematicBuilder()
     .name("out")
     .from_template(XOR)
-    .map('l', 'minecraft:lever[face=floor,powered=false]')
-    .map('░', 'minecraft:redstone_lamp[lit=false]')
-    .build())
+    .map("l", "minecraft:lever[face=floor,powered=false,facing=north]")
+    .map("░", "minecraft:redstone_lamp[lit=false]")
+    .build()
+)
 
 
 world = schematic.create_simulation_world()
-world.on_use_block(0, 1, 3) 
+world.on_use_block(0, 1, 3)
 world.tick(4)
 
 world.sync_to_schematic()
 schem = world.get_schematic()
 schem.to_mesh(resource_pack).save("artifacts/out.glb")
-schem.save("artifacts/stone.schem")
-schem.save("artifacts/stone.litematic")
+schem.save("artifacts/out.schem")
+schem.save("artifacts/out.litematic")
+schem.render_to_file(
+    resource_pack,
+    "artifacts/out.png",
+    nucleation.RenderConfig(
+        width=3840,
+        height=2160,
+        yaw=45.0, 
+        pitch=45.0, 
+        zoom=0.7,
+        fov=45.0,
+    ),
+)
 
-# for i in range(10):
-#     time.sleep(1)
-#     world.on_use_block(3, 1, 3) 
-#     world.tick(4)
-#     world.sync_to_schematic()
-#     world.get_schematic().to_mesh(resource_pack).save("artifacts/out.glb")
