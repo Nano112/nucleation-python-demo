2bcb8bfb59369561d0fdd0565b47b17e76e02ce3
Harrison <35530362+Nano112@users.noreply.github.com>
2026-02-23 02:21:35 +1100

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+import os
+from nucleation import Schematic, ResourcePack, SchematicBuilder
+resource_pack = ResourcePack.from_file("pack.zip")
+schem = Schematic("stone")
+
+XOR = ""
+with open("xor.txt", "r") as f:
+    XOR = f.read()
+    
+schematic = (SchematicBuilder()
+    .name("out")
+    .from_template(XOR)
+    .map('l', 'minecraft:lever[face=floor,powered=false]')
+    .map('░', 'minecraft:redstone_lamp[lit=false]')
+    .build())
+
+
+world = schematic.create_simulation_world()
+world.on_use_block(0, 1, 3) 
+world.tick(4)
+
+world.sync_to_schematic()
+world.get_schematic().to_mesh(resource_pack).save("artifacts/out.glb")
+
+
+# for i in range(10):
+#     time.sleep(1)
+#     world.on_use_block(3, 1, 3) 
+#     world.tick(4)
+#     world.sync_to_schematic()
+#     world.get_schematic().to_mesh(resource_pack).save("artifacts/out.glb")
